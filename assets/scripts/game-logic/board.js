50eaab95dca6b16411e3e4fe9e35fc60fa2a455f
RasyadiAbdoellah <rfabdoellah@gmail.com>
2017-12-21 14:06:36 -0500

    Refactor proto and board.js
    moved functions in board.js to proto.js and made it all object prototype
    functions. board.js now is exclusively for testing and initial board
    declaration. Board.js should probably be moved out of game-logic folder
    in future and used as touchpoint for API

--- assets/scripts/game-logic/board.js
+++ assets/scripts/game-logic/board.js
@@ -2,53 +2,72 @@ const Game = require('./proto')
 
 const board = new Game()
 
-// x always goes first. will be switched to o when play is called
-let currentPlayer = 'x'
-let turnCount = 0
-let winner
-
-const play = function (index) {
-  if (board.over !== true) {
-    if (!board.collisionCheck(index)) {
-      board.cells[index] = currentPlayer
-      if (currentPlayer === 'x') {
-        currentPlayer = 'o'
-      } else {
-        currentPlayer = 'x'
-      }
-    }
-    winner = board.winCheck()
-    console.log(winner)
-  } else {
-    console.log('game is over!')
-    if (winner !== 0) {
-      console.log(winner + ' is the winner!')
-    } else {
-      console.log('Stalemate!')
-    }
-  }
-}
-
-// test game
-play(0)
-turnCount++
-console.log(turnCount)
-play(4)
-turnCount++
-console.log(turnCount)
-play(6)
-turnCount++
-console.log(turnCount)
-play(1)
-turnCount++
-console.log(turnCount)
+// x always goes first. will be switched to o when board.play is called
+// test case 1
+board.play(0)
+board.play(4)
+board.play(6)
+board.play(1)
 // call below should return a winner
-play(3)
-turnCount++
-console.log(turnCount)
-// call below should prevent play
-play(5)
-turnCount++
-console.log(turnCount)
+board.play(3)
+// call below should prevent board.play
+board.play(5)
 console.log(board.cells)
 console.log(board.over)
+// clear and create new board
+board.clearBoard()
+// test case 2
+board.play(1)
+board.play(2)
+board.play(3)
+board.play(4)
+board.play(5)
+// should trigger win condition here
+board.play(6)
+// board.play() below should not return
+board.play(7)
+console.log(board.cells)
+console.log(board.over)
+
+board.clearBoard()
+
+// test 3
+board.play(0)
+board.play(3)
+board.play(4)
+board.play(5)
+// should trigger win here
+board.play(8)
+// board.play should not run
+board.play(7)
+console.log(board.cells)
+console.log(board.over)
+
+board.clearBoard()
+
+board.play(0)
+board.play(4)
+board.play(8)
+board.play(3)
+board.play(6)
+// should trigger win condition
+board.play(5)
+
+console.log(board.cells)
+console.log(board.over)
+board.clearBoard()
+
+board.play(0)
+board.play(4)
+board.play(1)
+board.play(2)
+board.play(6)
+board.play(3)
+board.play(5)
+board.play(7)
+board.play(8)
+
+
+console.log(board.cells)
+console.log(board.over)
+board.clearBoard()
--- assets/scripts/game-logic/proto.js
+++ assets/scripts/game-logic/proto.js
@@ -1,5 +1,4 @@
 // callback to check if element is empty or not
-
 const isNotEmpty = function (currentValue) {
   return (currentValue !== '')
 }
@@ -7,14 +6,50 @@ const isNotEmpty = function (currentValue) {
 const Game = function () {
   this.cells = ['', '', '', '', '', '', '', '', '']
   this.over = false
+  this.currentPlayer = 'x'
+  this.win = false
+  this.turnCount = 0
+}
+
+Game.prototype.play = function (index) {
+  this.turnCount++
+  console.log(this.turnCount)
+  if (this.over !== true) {
+    if (!this.collisionCheck(index)) {
+      this.cells[index] = this.currentPlayer
+      // win condition is assigned to a global variable
+      this.win = this.winCheck()
+      // this.currentPlayer is switched only if win is false
+      if (this.win !== true) {
+        this.currentPlayer === 'x' ? this.currentPlayer = 'o' : this.currentPlayer = 'x'
+      }
+    }
+    // console.log(this.win)
+  }
+  if (this.win === true || this.over === true) {
+    console.log('game is over!')
+    if (this.win !== 0) {
+      console.log(this.currentPlayer + ' is the winner!')
+    } else {
+      console.log('Stalemate!')
+    }
+  }
+}
+// this reset function
+Game.prototype.clearBoard = function () {
+  this.cells = ['', '', '', '', '', '', '', '', '']
+  this.over = false
+  this.currentPlayer = 'x'
+  this.win = false
+  this.turnCount = 0
 }
 // horizontalCheck checks if all values in a row match. assumes value is NOT undefined
 Game.prototype.horizontalCheck = function (index) {
   if (this.cells[index] === this.cells[index + 1] && this.cells[index] === this.cells[index + 2]) {
     // change current game condition
     this.over = true
-    // returns the winning token
-    return this.cells[index]
+    // returns true value
+    return true
   }
 }
 // verticalCheck checks if all values in a column match. assumes value is NOT undefined
@@ -22,8 +57,8 @@ Game.prototype.verticalCheck = function (index) {
   if (this.cells[index] === this.cells[index + 3] && this.cells[index] === this.cells[index + 6]) {
     // change current game condition
     this.over = true
-    // returns winning token
-    return this.cells[index]
+    // returns true value
+    return true
   }
 }
 
@@ -31,13 +66,13 @@ Game.prototype.winCheck = function () {
   // winCheck checks for vertical, horizontal, or diagonal matches
   // toggles the over property if cells is completely filled or win condition is met
   // returns winning side
-  console.log('checking winner')
+  // console.log('checking winner')
   // horizontal check
   for (let i = 0; i < this.cells.length; i += 3) {
-    console.log('horizontal check')
-    console.log('current value: ', this.cells[i])
+    // console.log('horizontal check')
+    // console.log('current value: ', this.cells[i])
     // checks for undefined
-    console.log('isNotEmpty:', isNotEmpty(this.cells[i]))
+    // console.log('isNotEmpty:', isNotEmpty(this.cells[i]))
     if (isNotEmpty(this.cells[i])) {
       if (this.horizontalCheck(i)) {
         return this.horizontalCheck(i)
@@ -46,10 +81,10 @@ Game.prototype.winCheck = function () {
   }
   // vertical check
   for (let i = 0; i < 3; i++) {
-    console.log('vertical check')
-    console.log('current value: ', this.cells[i])
-    // checks for undefined
-    console.log('isNotEmpty:', isNotEmpty(this.cells[i]))
+    // console.log('vertical check')
+    // console.log('current value: ', this.cells[i])
+    // // checks for undefined
+    // console.log('isNotEmpty:', isNotEmpty(this.cells[i]))
     if (isNotEmpty(this.cells[i])) {
       if (this.verticalCheck(i)) {
         return this.verticalCheck(i)
@@ -58,22 +93,22 @@ Game.prototype.winCheck = function () {
   }
   // diagonal from top left to bottom right check
   if (isNotEmpty(this.cells[0]) && (this.cells[0] === this.cells[4] && this.cells[0] === this.cells[8])) {
-    console.log('diag 1 check')
-    console.log('current value: ', this.cells[0])
+    // console.log('diag 1 check')
+    // console.log('current value: ', this.cells[0])
     // changes current game condition
     this.over = true
-    // returns winning token
-    return this.cells[0]
+    // returns true if won
+    return true
   }
   // diagonal from top right to bottom left check
-  if (isNotEmpty(this.cells[0]) && (this.cells[2] === this.cells[4] && this.cells[0] === this.cells[6])) {
-    console.log('diag 1 check')
-    console.log('current value: ', this.cells[2])
+  if (isNotEmpty(this.cells[2]) && (this.cells[2] === this.cells[4] && this.cells[2] === this.cells[6])) {
+    // console.log('diag 2 check')
+    // console.log('current value: ', this.cells[2])
     // changes current game condition
     this.over = true
-    console.log(this.over)
-    // returns winning token
-    return this.cells[2]
+    // console.log(this.over)
+    // returns true if won
+    return true
   }
   // checks for stalemate
   if (this.cells.every(isNotEmpty)) {
