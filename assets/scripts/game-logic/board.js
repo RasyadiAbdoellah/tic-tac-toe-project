0c825d4556a8344391f2622853bdf3981dd5e0e0
RasyadiAbdoellah <rfabdoellah@gmail.com>
2017-12-21 12:16:47 -0500

    Base game logic close to completion
    prototype functions work as predicted. need to test multiple cases.
    play() function needs to stop play as soon as over bool is switched to
    true.

--- assets/scripts/game-logic/board.js
+++ assets/scripts/game-logic/board.js
@@ -4,26 +4,51 @@ const board = new Game()
 
 // x always goes first. will be switched to o when play is called
 let currentPlayer = 'x'
+let turnCount = 0
+let winner
 
 const play = function (index) {
-  if (!board.collisionCheck(index)) {
-    board.cells[index] = currentPlayer
-    if (currentPlayer === 'x') {
-      currentPlayer = 'o'
+  if (board.over !== true) {
+    if (!board.collisionCheck(index)) {
+      board.cells[index] = currentPlayer
+      if (currentPlayer === 'x') {
+        currentPlayer = 'o'
+      } else {
+        currentPlayer = 'x'
+      }
+    }
+    winner = board.winCheck()
+    console.log(winner)
+  } else {
+    console.log('game is over!')
+    if (winner !== 0) {
+      console.log(winner + ' is the winner!')
     } else {
-      currentPlayer = 'x'
+      console.log('Stalemate!')
     }
   }
-  board.winCheck()
 }
 
 // test game
 play(0)
+turnCount++
+console.log(turnCount)
 play(4)
+turnCount++
+console.log(turnCount)
 play(6)
+turnCount++
+console.log(turnCount)
 play(1)
+turnCount++
+console.log(turnCount)
 // call below should return a winner
 play(3)
+turnCount++
+console.log(turnCount)
 // call below should prevent play
 play(5)
+turnCount++
+console.log(turnCount)
 console.log(board.cells)
+console.log(board.over)
--- assets/scripts/game-logic/proto.js
+++ assets/scripts/game-logic/proto.js
@@ -13,9 +13,8 @@ Game.prototype.horizontalCheck = function (index) {
   if (this.cells[index] === this.cells[index + 1] && this.cells[index] === this.cells[index + 2]) {
     // change current game condition
     this.over = true
-    console.log(this.over)
-    // returns true if match
-    return true
+    // returns the winning token
+    return this.cells[index]
   }
 }
 // verticalCheck checks if all values in a column match. assumes value is NOT undefined
@@ -23,9 +22,8 @@ Game.prototype.verticalCheck = function (index) {
   if (this.cells[index] === this.cells[index + 3] && this.cells[index] === this.cells[index + 6]) {
     // change current game condition
     this.over = true
-    console.log(this.over)
     // returns winning token
-    return true
+    return this.cells[index]
   }
 }
 
@@ -33,66 +31,56 @@ Game.prototype.winCheck = function () {
   // winCheck checks for vertical, horizontal, or diagonal matches
   // toggles the over property if cells is completely filled or win condition is met
   // returns winning side
-  let winner
   console.log('checking winner')
-
-  if (this.over !== true) {
-    // horizontal check
-    for (let i = 0; i < this.cells.length; i += 3) {
-      console.log('horizontal check')
-      console.log('current value: ', this.cells[i])
-      // checks for undefined
-      console.log('isNotEmpty:', isNotEmpty(this.cells[i]))
-      if (isNotEmpty(this.cells[i])) {
-        winner = this.horizontalCheck(i)
+  // horizontal check
+  for (let i = 0; i < this.cells.length; i += 3) {
+    console.log('horizontal check')
+    console.log('current value: ', this.cells[i])
+    // checks for undefined
+    console.log('isNotEmpty:', isNotEmpty(this.cells[i]))
+    if (isNotEmpty(this.cells[i])) {
+      if (this.horizontalCheck(i)) {
+        return this.horizontalCheck(i)
       }
     }
-    // vertical check
-    for (let i = 0; i < 3; i++) {
-      console.log('vertical check')
-      console.log('current value: ', this.cells[i])
-      // checks for undefined
-      console.log('isNotEmpty:', isNotEmpty(this.cells[i]))
-      if (isNotEmpty(this.cells[i])) {
-        winner = this.verticalCheck(i)
+  }
+  // vertical check
+  for (let i = 0; i < 3; i++) {
+    console.log('vertical check')
+    console.log('current value: ', this.cells[i])
+    // checks for undefined
+    console.log('isNotEmpty:', isNotEmpty(this.cells[i]))
+    if (isNotEmpty(this.cells[i])) {
+      if (this.verticalCheck(i)) {
+        return this.verticalCheck(i)
       }
     }
-    // diagonal from top left to bottom right check
-    if (isNotEmpty(this.cells[0]) && (this.cells[0] === this.cells[4] && this.cells[0] === this.cells[8])) {
-      console.log('diag 1 check')
-      console.log('current value: ', this.cells[0])
-      // changes current game condition
-      this.over = true
-      // returns winning token
-      winner = this.cells[0]
-      return winner
-    }
-    // diagonal from top right to bottom left check
-    if (isNotEmpty(this.cells[0]) && (this.cells[2] === this.cells[4] && this.cells[0] === this.cells[6])) {
-      console.log('diag 1 check')
-      console.log('current value: ', this.cells[2])
-      // changes current game condition
-      this.over = true
-      console.log(this.over)
-      // returns winning token
-      winner = this.cells[2]
-      return winner
-    }
-    // checks for stalemate
-    if (this.cells.every(isNotEmpty)) {
-      console.log('stalemate check')
-      this.over = true
-      // returns stalemate token
-      winner = 0
-      return winner
-    }
-  } else {
-    console.log('game is over!')
-    if (winner !== 0) {
-      console.log(winner + ' is the winner!')
-    } else {
-      console.log('Stalemate!')
-    }
+  }
+  // diagonal from top left to bottom right check
+  if (isNotEmpty(this.cells[0]) && (this.cells[0] === this.cells[4] && this.cells[0] === this.cells[8])) {
+    console.log('diag 1 check')
+    console.log('current value: ', this.cells[0])
+    // changes current game condition
+    this.over = true
+    // returns winning token
+    return this.cells[0]
+  }
+  // diagonal from top right to bottom left check
+  if (isNotEmpty(this.cells[0]) && (this.cells[2] === this.cells[4] && this.cells[0] === this.cells[6])) {
+    console.log('diag 1 check')
+    console.log('current value: ', this.cells[2])
+    // changes current game condition
+    this.over = true
+    console.log(this.over)
+    // returns winning token
+    return this.cells[2]
+  }
+  // checks for stalemate
+  if (this.cells.every(isNotEmpty)) {
+    console.log('stalemate check')
+    this.over = true
+    // returns stalemate token
+    return 0
   }
 }
 // collisionCheck takes index arg and checks if this.cells[index] is empty.
