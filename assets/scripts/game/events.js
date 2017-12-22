7cfd7fc81e88af9c2074bf2a62db48b373f13bb1
RasyadiAbdoellah <rfabdoellah@gmail.com>
2017-12-22 12:30:28 -0500

    Tie game logic to events.js
    leave board.js alone for now. Will move board interaction functions to
    from events.js to board.js if code becomes too long in the future.

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -1,9 +1,10 @@
 // const getFormFields = require('../../lib/get-form-fields')
+const board = require('./logic/proto')
 
 const onCellClick = function (event) {
   // console.log(event.target)
   const val = $(event.target).attr('data-value')
-  console.log('cell ' + val + ' clicked')
+  board.play(val)
 }
 
 const addHandler = function (event) {
