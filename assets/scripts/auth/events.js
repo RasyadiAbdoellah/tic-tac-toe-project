e5975d72b68a484cab2a61db1959ff3d314050e9
RasyadiAbdoellah <rfabdoellah@gmail.com>
2017-12-22 15:20:57 -0500

    Add successful sign up message.
    also fixed bugs in hide/show/toggle process

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -33,9 +33,11 @@ const onSignOut = function (event) {
 
 const addHandler = function (event) {
   // hide signed-in elements on page load
+  $('#sign-in').hide()
   $('#display-sign-up').hide()
   $('#change-password').hide()
   $('#sign-out').hide()
+  $('#sign-form-message').hide()
 
   $('#sign-up').on('submit', onSignUp)
   $('#sign-in').on('submit', onSignIn)
--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,13 +2,27 @@
 const store = require('../store')
 
 const onSignUpSuccess = function (data) {
-  store.user = data.user
-  console.log(store.user)
+  console.log(data)
+  // explicitly hide and show the elements we want
+  $('#sign-up').hide() // hide sign-up field on success
+  $('#sign-in').show() // show sign in field on success
+
+  // display the right toggle message on success
+  $('#display-sign-in').hide()
+  $('#display-sign-up').show()
+
+  // show form message
+  $('#sign-form-message').show()
+  $('#sign-form-message').text('Sign up successful! Sign in below')
 }
 
 const onSignInSuccess = function (data) {
   store.user = data.user
   console.log(store.user)
+  // explicitely hide the form message on success
+  $('#sign-form-message').hide()
+
+  // toggle the a signed in user's functionality
   $('#change-password').toggle()
   $('#sign-out').toggle()
   $('#sign-in').toggle()
@@ -29,10 +43,18 @@ const onSignOutSuccess = function () {
 }
 
 const signFormToggle = function () {
+  //  always hides the alert message
+  $('#sign-form-message').hide()
+
+  // toggle functionality below
   $('#sign-up').toggle()
   $('#sign-in').toggle()
   $('#display-sign-in').toggle()
   $('#display-sign-up').toggle()
+}
+
+const onFailure = function () {
+
 }
 
 module.exports = {
