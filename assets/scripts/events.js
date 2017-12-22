8a8dfc9bda5994fe979477a4828370f7e369ee84
RasyadiAbdoellah <rfabdoellah@gmail.com>
2017-12-22 11:13:53 -0500

    create onCellClick callback for cell event handler

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,7 +1,10 @@
+const getFormFields = require('../../lib/get-form-fields')
+
 const onSignUp = function (event) {
   event.preventDefault()
   console.log('sign up triggered')
-  console.log(event.target)
+  const data = getFormFields(event.target)
+  console.log(data)
 }
 
 const onSignIn = function (event) {
@@ -16,10 +19,17 @@ const onSignOut = function (event) {
   console.log(event.target)
 }
 
+const onCellClick = function (event) {
+  // console.log(event.target)
+  const val = $(event.target).attr('value')
+  console.log('cell ' + val + ' clicked')
+}
+
 const addHandler = function (event) {
   $('#sign-up').on('submit', onSignUp)
   $('#sign-in').on('submit', onSignIn)
   $('#sign-out').on('submit', onSignOut)
+  $('.cell').on('click', onCellClick)
 }
 
 module.exports = {
