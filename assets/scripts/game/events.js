dd5670899c117a30046fa49737ff3130e0a5119e
RasyadiAbdoellah <rfabdoellah@gmail.com>
2017-12-23 00:58:41 -0500

    add player alert and tweak responsiveness

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -6,15 +6,28 @@ const board = new Game()
 
 const onCellClick = function (event) {
   // console.log(event.target)
+  // const currentCell = $(this)
   const val = $(this).attr('data-value')
   if (!board.over && !board.collisionCheck(val)) {
-    $(this).text(board.currentPlayer)
+    $(this).text(board.currentPlayer.toUpperCase())
     board.play(val)
+    $('#game-alert').removeClass().addClass('alert alert-info').text(board.currentPlayer.toUpperCase() + ' player\'s turn')
+  } else if (!board.over && board.collisionCheck(val)) {
+    $('#game-alert').removeClass().addClass('alert alert-danger').text('Please select a valid space')
   }
+  if (board.winCheck()) {
+    $('#game-alert').removeClass().addClass('alert alert-info').text(board.currentPlayer.toUpperCase() + ' player won!')
+  } else if (board.winCheck() === 0) {
+    $('#game-alert').removeClass().addClass('alert alert-info').text('Stalemate!')
+  }
+
+  $('#game-alert').fadeIn(100).delay(1000).fadeOut(100)
   console.log(board.cells)
 }
 
 const addHandler = function (event) {
+  $('#game-alert').hide()
+
   $('.cell').on('click', onCellClick)
 }
 
