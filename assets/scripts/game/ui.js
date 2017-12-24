188c3ce2065c02aca048a9b6f872b017ab0fa66f
RasyadiAbdoellah <rfabdoellah@gmail.com>
2017-12-23 23:40:17 -0500

    refactor events.js
    moved all ui related functionality to ui.js

---
+++ assets/scripts/game/ui.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const toggleCurrentPlayerAlert = function (board) {
+  $('#game-alert').removeClass().addClass('alert alert-info').text(board.currentPlayer.toUpperCase() + ' player\'s turn.')
+}
+
+const showInvalidMoveWarning = function () {
+  $('#game-alert').removeClass().addClass('alert alert-danger').text('Please select a valid space.')
+}
+
+const showWinMessage = function (board) {
+  $('#game-alert').removeClass().addClass('alert alert-info').text('Game over! ' + board.currentPlayer.toUpperCase() + ' player won!')
+}
+
+const showStalemateMessage = function () {
+  $('#game-alert').removeClass().addClass('alert alert-info').text('Stalemate!')
+}
+
+const resetBoardUi = function () {
+  $('.cell').children('.cell-content').text('')
+  $('#game-alert').removeClass().addClass('alert alert-info').text('New game! X Player\'s turn.')
+}
+
+module.exports = {
+  toggleCurrentPlayerAlert,
+  showInvalidMoveWarning,
+  showWinMessage,
+  showStalemateMessage,
+  resetBoardUi
+}
