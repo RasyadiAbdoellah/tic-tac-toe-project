fed1faa9268a5406997c5bdd358b6491f409f79f
RasyadiAbdoellah <rfabdoellah@gmail.com>
2017-12-28 00:08:33 -0500

    UI Tweaks
    Changed the look of sign in/register forms using bootstrap panel

--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -2,27 +2,27 @@
 const store = require('../store')
 
 const toggleCurrentPlayerAlert = function (board) {
-  $('#game-alert').removeClass().addClass('alert alert-info').text(board.currentPlayer.toUpperCase() + ' player\'s turn.')
+  $('#game-alert').removeClass().addClass('game-message text-info').text(board.currentPlayer.toUpperCase() + ' player\'s turn.')
 }
 
 const showInvalidMoveWarning = function () {
-  $('#game-alert').removeClass().addClass('alert alert-danger').text('Please select a valid space.')
+  $('#game-alert').removeClass().addClass('game-message text-danger').text('Please select a valid space!')
 }
 
 const showWinMessage = function (board) {
-  $('#game-alert').removeClass().addClass('alert alert-info').text('Game over! ' + board.currentPlayer.toUpperCase() + ' player won!')
+  $('#game-alert').removeClass().addClass('game-message text-info').text('Game over! ' + board.currentPlayer.toUpperCase() + ' player won!')
 }
 
 const showStalemateMessage = function () {
-  $('#game-alert').removeClass().addClass('alert alert-info').text('Stalemate!')
+  $('#game-alert').removeClass().addClass('game-message text-danger').text('Stalemate!')
 }
 
 const resetBoardUi = function () {
   $('.cell').children('.cell-content').text('')
   if (store.user === undefined) {
-    $('#game-alert').removeClass().addClass('alert alert-info').text('New local game! Sign in to save progress. X Player\'s turn.')
+    $('#game-alert').removeClass().addClass('game-message text-info').text('New local game! Sign in to save progress. X Player\'s turn.')
   } else {
-    $('#game-alert').removeClass().addClass('alert alert-info').text('New game! Saving progress to account. X Player\'s turn.')
+    $('#game-alert').removeClass().addClass('game-message text-info').text('New game! Saving progress to account. X Player\'s turn.')
   }
 }
 
